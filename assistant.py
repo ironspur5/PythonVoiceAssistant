b4b5e2ce6d1819d2b2ebb93f41ea6d9cbe659523
patrickguha <p.ironspur@gmail.com>
2019-02-18 23:57:24 -0600

    9th commit; fixed date and time; now runs infinitely; unintentionally always says time and joke still (think this is because the request call)

--- assistant.py
+++ assistant.py
@@ -2,7 +2,7 @@
 
 import pyaudio
 import speech_recognition as sr
-from time import ctime
+from datetime import datetime
 import time
 import os
 from gtts import gTTS
@@ -23,8 +23,7 @@ def recordAudio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
-
-    # Speech recognition using Google Speech Recognition
+        # Speech recognition using Google Speech Recognition
     data = ""
     try:
         # Uses the default API key
@@ -42,23 +41,43 @@ def recordAudio():
 def jarvis(data):
     if "how are you" in data:
         speak("I am fine")
-        print("I am fine")
 
-    if "time" or "date" in data:
-        speak(ctime())
-        print(ctime())
+    if "time" in data:
+        tm = datetime.now()
+        speak(tm.strftime("%I:%M%p"))
+
+    if "date" in data:
+        now = datetime.now()
+        speak(now.strftime("%A,%d %B,%Y"))
 
     if "news" in data:
         webbrowser.open('https://news.google.com')
 
+    # adding the joke part always says a joke and messes up
+    # maybe need to download archive as a json and import to avoid request
+    '''
     if "joke" or "jokes" in data:
         response = requests.get('https://official-joke-api.appspot.com/random_joke')
         joke = json.loads(response.text)
         speak(joke["setup"])
         speak(joke["punchline"])
+    '''
+
+    if "where is" in data:
+        data = data.split(" ")
+        location = data[2]
+        speak("Hold on, I will show you where " + location + " is.")
 
 
-# main
+
+# initialization
+time.sleep(2)
+speak("Hi, what can I do for you?")
+while 1:
+    data = recordAudio()
+    jarvis(data)
+
+'''
 while 1:
     usrinput = input("Press A to ask me something or B to quit: ")
     if usrinput == "A":
@@ -67,6 +86,10 @@ while 1:
         jarvis(data)
     elif usrinput == "B":
         break
+'''
+
+
+
 
 
 
